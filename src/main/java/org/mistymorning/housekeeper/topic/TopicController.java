8b8f701f6c09f6c0e21791f2d0644d7339e900de
frogfather <j.a.campbell@icloud.com>
2019-05-20 22:20:54 +0100

    add rest methods and injected service

--- src/main/java/org/mistymorning/housekeeper/topic/TopicController.java
+++ src/main/java/org/mistymorning/housekeeper/topic/TopicController.java
@@ -1,6 +1,7 @@
 package org.mistymorning.housekeeper.topic;
 
 import java.util.List;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,16 @@ public class TopicController {
 	@RequestMapping(method=RequestMethod.POST, value="/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
 	}
 	
 }
